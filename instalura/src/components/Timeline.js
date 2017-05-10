de46e2591f2ee88537971edb45fd05d053ff3305
Gustavo Perenciolo <gustavo@blackowl.com.br>
2017-05-10 18:02:16 -0300

    Criando Login

--- instalura/src/components/Timeline.js
+++ instalura/src/components/Timeline.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
+
+    constructor() {
+        super();
+        this.state = { fotos: [] };
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:8080/api/public/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+            .then(response => response.json())
+            .then(fotos => this.setState({ fotos: fotos }))
+            ;
+    }
+
     render() {
         return (
             <div className="fotos container">
-                <FotoItem />
-                <FotoItem />
+                {
+                    this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} />)
+                }
             </div>
         );
     }
--- instalura/src/index.js
+++ instalura/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; 
+import App from './App';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import { Router, Route, browserHistory } from 'react-router';
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
+
+function verificaAutenticacao(nextState, replace) {
+  if (localStorage.getItem('auth-token') === null) {
+    replace('/msg=Faça o login para acessar.');
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path='/' component={Login} />
+    <Route path='/timeline' component={App} onEnter={verificaAutenticacao} />
+    <Route path='/logout' component={Logout} />
+  </Router>,
   document.getElementById('root')
 );
